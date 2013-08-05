0e383bec83908e811cf1ecc88a84bb46ced668f2
Utkarsh <utkarshsins@gmail.com>
2013-08-05 14:23:55 +0800

    issue-5 admin thread

---
+++ src/bkground/server/terminal/Skeleton.java
@@ -0,0 +1,17 @@
+package bkground.server.terminal;
+
+import bkground.server.terminal.listeners.ListenerAdministrator;
+
+public class Skeleton {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		ListenerAdministrator admin = new ListenerAdministrator();
+		admin.run();
+
+	}
+
+}
---
+++ src/bkground/server/terminal/listeners/ListenerAdministrator.java
@@ -0,0 +1,38 @@
+package bkground.server.terminal.listeners;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class ListenerAdministrator implements Runnable {
+	private static final String ADMIN_TITLE = "Choose from the following options : ";
+	private static final String ADMIN_OPTIONS[] = { "1. Stop the server" };
+
+	@Override
+	public void run() {
+
+		int optionVal = 0;
+
+		Scanner scanner = new Scanner(System.in);
+
+		while (true) {
+			System.out.println(ADMIN_TITLE);
+			for (String option : ADMIN_OPTIONS) {
+				System.out.println(option);
+			}
+
+			try {
+				optionVal = scanner.nextInt();
+			} catch (NoSuchElementException | IllegalStateException e) {
+				e.printStackTrace();
+			}
+
+			if (optionVal == 1) {
+				System.err.println("Shutting down server.");
+				break;
+			}
+		}
+
+		scanner.close();
+
+	}
+}
